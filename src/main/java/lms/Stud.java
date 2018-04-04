a77b6c17fdfdebdc07828a44c253ad71e24c6faa
roshna9 <md.roshna@gmail.com>
2018-04-04 10:48:35 +0530

    this is first commit

---
+++ src/main/java/lms/Stud.java
@@ -0,0 +1,8 @@
+package lms;
+
+public class Stud {
+public String Sample() {
+	return "this is first class";
+	
+}
+}
---
+++ src/test/java/lms/S1test.java
@@ -0,0 +1,14 @@
+package lms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class S1test {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
